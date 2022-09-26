42447f344f52eb43d078f420e3a35e259983327f
joshj1108 <joshua.a.jackson.1990@gmail.com>
2022-09-26 11:50:11 -0700

    Add function for 5 game loop. Keep score, report winner

--- javascript.js
+++ javascript.js
@@ -22,18 +22,49 @@ function playRound(playerSelection, computerSelection){
     if (playerChoice == computerSelection){
         return "Tie"
     } else if (playerChoice == 'rock' && computerSelection == 'paper'){
+        computerScore++;
         return "You lose, " + computerSelection + " beats " + playerChoice;
     } else if (playerChoice == 'rock' && computerSelection == 'scissors'){
+        playerScore++;
         return "You win, " + playerChoice + " beats " + computerSelection;
     } else if (playerChoice == 'paper' && computerSelection == 'scissors'){
+        computerScore++;
         return "You lose, " + computerSelection + " beats " + playerChoice;
     } else if (playerChoice == 'paper' && computerSelection == 'rock'){
+        playerScore++;
         return "You win, " + playerChoice + " beats " + computerSelection;
     } else if (playerChoice == 'scissors' && computerSelection == 'rock'){
+        computerScore++;
         return "You lose, " + computerSelection + " beats " + playerChoice;
     } else if (playerChoice == 'scissors' && computerSelection == 'paper'){
+        playerScore++;
         return "You win, " + playerChoice + " beats " + computerSelection;
+    } else if (playerChoice !== 'rock' || 'paper' || 'scissors'){
+        return "You can't use that weapon, choose again"
     }
 }
 
-console.log(playRound('paper',getComputerChoice()))
+let playerScore = 0;
+let computerScore = 0;
+let playerSelection = prompt("Rock, Paper or Scissors? ")
+
+function game(){
+    for (let i = 0; i < 5; i++){
+        playRound(playerSelection,getComputerChoice())
+    }
+
+    if (playerScore > computerScore){
+        return "Congratulations, you win"
+    } else if (playerScore < computerScore){
+        return "You lost, better luck next time"
+    } else if (playerScore == computerScore){
+        return "Tie game"
+    }
+}
+
+
+console.log(game());
+console.log(computerScore);
+console.log(playerScore);
+
+
